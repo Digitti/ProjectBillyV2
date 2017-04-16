9d67498420b21502906038cda43ec893bf5fe413
Kevin <kevin.adagba@gmail.com>
2017-04-16 17:44:14 +0200

    liaison  ihm code v1

--- src/src/IHMV3.java
+++ src/src/IHMV3.java
@@ -7,44 +7,41 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
-import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import java.awt.event.ActionListener;
-import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.awt.event.ActionEvent;
 
-import javax.swing.JToggleButton;
-import javax.swing.JTextPane;
-import javax.swing.JInternalFrame;
-import javax.swing.JDesktopPane;
-import javax.swing.JTabbedPane;
+
 import java.awt.FlowLayout;
-import java.awt.TextArea;
-import java.awt.CardLayout;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.SwingConstants;
+
 import javax.swing.JTextArea;
-import javax.swing.JLayeredPane;
-import javax.swing.JList;
+
 import javax.swing.JTable;
-import javax.swing.DropMode;
+import javax.swing.JToggleButton;
+
 
 
 public class IHMV3 implements MyFrame {
 
 	private JFrame frmBillyProjectV;
-	private JTextField textField_ipServer;
-	private JTextField textField_listenPort;
+	private JTextField textField_ipDst;
+	private JTextField textField_listenUDP;
 	private JTextField textField_File;
-	private JTextField textField_ipLocal;
+	private JTextField textField_ipSrc;
 	private JTable table_Tank;
+	private JTextField textField_listenTCP;
+	private JTextField textField_PathDir;
+	private JTextField textField_sendUDP;
+	private JTextField textField_sendTCP;
 
 	/**
 	 * Launch the application.
@@ -68,8 +65,6 @@ public class IHMV3 implements MyFrame {
 	 */
 	public IHMV3() throws UnknownHostException {
 		initialize();
-		String ip = InetAddress.getLocalHost().toString();
-		textField_ipLocal.setText(ip.substring(ip.indexOf("1")));
 	}
 
 	/**
@@ -91,45 +86,75 @@ public class IHMV3 implements MyFrame {
 		JMenuItem mntmQuit = new JMenuItem("Quit");
 		mnMenu.add(mntmQuit);
 		
-		JPanel panel_Settings = new JPanel();
-		panel_Settings.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panel_file = new JPanel();
+		panel_file.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblNewLabel_2 = new JLabel("IP Local : ");
-		panel_Settings.add(lblNewLabel_2);
+		JLabel lblNewLabel_2 = new JLabel("IP Source : ");
+		panel_file.add(lblNewLabel_2);
 		
-		textField_ipLocal = new JTextField();
-		textField_ipLocal.setEditable(false);
-		panel_Settings.add(textField_ipLocal);
-		textField_ipLocal.setColumns(10);
+		textField_ipSrc = new JTextField();
+		panel_file.add(textField_ipSrc);
+		textField_ipSrc.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("IP Server :");
-		panel_Settings.add(lblNewLabel);
+		JLabel lblNewLabel = new JLabel("IP Destination :");
+		panel_file.add(lblNewLabel);
 		
-		textField_ipServer = new JTextField();
-		panel_Settings.add(textField_ipServer);
-		textField_ipServer.setColumns(10);
+		textField_ipDst = new JTextField();
+		panel_file.add(textField_ipDst);
+		textField_ipDst.setColumns(10);
 		
-		JCheckBox CheckBox_ipV6 = new JCheckBox("@IPV6");
-		panel_Settings.add(CheckBox_ipV6);
+		JLabel lblNewLabel_1 = new JLabel("UDP Port L :");
+		panel_file.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_1 = new JLabel("Listen Port :");
-		panel_Settings.add(lblNewLabel_1);
+		textField_listenUDP = new JTextField();
+		panel_file.add(textField_listenUDP);
+		textField_listenUDP.setColumns(10);
 		
-		textField_listenPort = new JTextField();
-		panel_Settings.add(textField_listenPort);
-		textField_listenPort.setColumns(10);
+		JLabel lblS = new JLabel("S :");
+		panel_file.add(lblS);
 		
-		JLabel lblSearchFile = new JLabel("Search File :");
-		panel_Settings.add(lblSearchFile);
+		textField_sendUDP = new JTextField();
+		textField_sendUDP.setColumns(10);
+		panel_file.add(textField_sendUDP);
 		
-		textField_File = new JTextField();
-		panel_Settings.add(textField_File);
-		textField_File.setColumns(10);
+		JLabel lblTcpPort = new JLabel("TCP Port L :");
+		panel_file.add(lblTcpPort);
 		
-		JCheckBox CheckBox_hashFile = new JCheckBox("Hash File");
-		panel_Settings.add(CheckBox_hashFile);
+		textField_listenTCP = new JTextField();
+		textField_listenTCP.setColumns(10);
+		panel_file.add(textField_listenTCP);
 		
-		JPanel panel_Task = new JPanel();
+		JToggleButton btnSet_Config = new JToggleButton("Set");
+		btnSet_Config.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// recuperation des donnee et creation de la trame udpRequest
+				String ipSrc = textField_ipSrc.getText();
+				String ipDst = textField_ipDst.getText();
+				String udpPort = textField_listenUDP.getText();
+				String tcpPort = textField_listenTCP.getText();
+				String sUdpPort = textField_sendUDP.getText();
+				String sTcpPort = textField_sendTCP.getText();
+				String pathDir = textField_PathDir.getText();
+				
+				// UDP
+				int listenUdp = Integer.parseInt(udpPort);
+				int listenTcp = Integer.parseInt(tcpPort);
+				int sendUdp = Integer.parseInt(sUdpPort);
+				int sendTcp = Integer.parseInt(sTcpPort);
+			
+				// set objet mainApplication
+				
+			}
+		});
+		
+		JLabel lblS_1 = new JLabel("S :");
+		panel_file.add(lblS_1);
+		
+		textField_sendTCP = new JTextField();
+		textField_sendTCP.setColumns(10);
+		panel_file.add(textField_sendTCP);
+		panel_file.add(btnSet_Config);
 		JLabel lblLog = new JLabel("Log :");
 		
 		JLabel lblTank = new JLabel("Tank :");
@@ -138,59 +163,70 @@ public class IHMV3 implements MyFrame {
 		
 		JTextArea textArea_Log = new JTextArea();
 		textArea_Log.setEditable(false);
+		
+		JPanel panel = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(frmBillyProjectV.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel_Settings, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 953, Short.MAX_VALUE)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblTank)
-					.addContainerGap(913, Short.MAX_VALUE))
-				.addComponent(panel_Task, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 953, Short.MAX_VALUE)
-				.addGroup(groupLayout.createSequentialGroup()
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addComponent(panel_file, GroupLayout.DEFAULT_SIZE, 1049, Short.MAX_VALUE)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblLog)
-					.addContainerGap(919, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(1015, Short.MAX_VALUE))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(table_Tank, GroupLayout.DEFAULT_SIZE, 933, Short.MAX_VALUE)
+					.addComponent(textArea_Log, GroupLayout.DEFAULT_SIZE, 1029, Short.MAX_VALUE)
 					.addContainerGap())
-				.addGroup(groupLayout.createSequentialGroup()
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(textArea_Log, GroupLayout.DEFAULT_SIZE, 933, Short.MAX_VALUE)
+					.addComponent(lblTank)
+					.addContainerGap(1009, Short.MAX_VALUE))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(table_Tank, GroupLayout.DEFAULT_SIZE, 1029, Short.MAX_VALUE)
 					.addContainerGap())
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 1049, Short.MAX_VALUE)
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(panel_Settings, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(panel_file, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblLog)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(textArea_Log, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblTank)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(table_Tank, GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_Task, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
+					.addComponent(table_Tank, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		
-		// Declaration de trame de requete
-		frameUdpRequest frUR = new frameUdpRequest();
+		JLabel lblPathDirectory = new JLabel("Path Directory :");
+		panel.add(lblPathDirectory);
 		
-		JButton btnSend = new JButton("Send");
-		btnSend.addActionListener(new ActionListener() {
+		textField_PathDir = new JTextField();
+		panel.add(textField_PathDir);
+		textField_PathDir.setColumns(10);
+		
+		JLabel lblSearchFile = new JLabel("Search File :");
+		panel.add(lblSearchFile);
+		
+		textField_File = new JTextField();
+		panel.add(textField_File);
+		textField_File.setColumns(10);
+		
+		JCheckBox CheckBox_hashFile = new JCheckBox("Hash File");
+		panel.add(CheckBox_hashFile);
+		
+		JButton btnSend_File = new JButton("Send");
+		panel.add(btnSend_File);
+		btnSend_File.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-					// recuperation des donnee et creation de la trame udpRequest
-					String ip = textField_ipServer.getText();
-					String port = textField_listenPort.getText();
-					String nameOrHash = textField_File.getText();
-					int pos = 0;
-					
-					// Verification de l'adresse ip
-					if(CheckBox_ipV6.isSelected())
+					/*if(CheckBox_ipV6.isSelected())
 					{
 						// verification que l'addresse est au format ipv6
 						if(ip.matches("([0-9a-f]+)\\:([0-9a-f]+)\\:([0-9a-f]+)\\:([0-9a-f]+)\\:([0-9a-f]+)\\:([0-9a-f]+)\\:([0-9a-f]+)\\:([0-9a-f]+)") == true)
@@ -239,10 +275,15 @@ public class IHMV3 implements MyFrame {
 						frUR.RequestType = REQUESTTYPE.NameRequest;
 						frUR.nameOrHash = nameOrHash.getBytes();
 					}
-				}
-			});
+				}*/
+				
+				
+				
+			}
+		});
 		
-		panel_Settings.add(btnSend);
+		// Declaration de trame de requete
+		frameUdpRequest frUR = new frameUdpRequest();
 		frmBillyProjectV.getContentPane().setLayout(groupLayout);
 	}
 }
